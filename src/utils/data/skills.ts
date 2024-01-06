2a560f629f8200cfa27dbf5b6596661fc7dc1433
zukicode <zukicode@gmail.com>
2024-01-07 00:02:55 +0200

    Add: another skills

--- src/utils/data/skills.ts
+++ src/utils/data/skills.ts
@@ -1,12 +1,15 @@
-import htmlIcon from 'assets/images/skills/html.png';
 import cssIcon from 'assets/images/skills/css.png';
-import scssIcon from 'assets/images/skills/scss.png';
+import expressjsIcon from 'assets/images/skills/ex.png';
+import firebaseIcon from 'assets/images/skills/firebase.png';
+import gitIcon from 'assets/images/skills/git.png';
+import htmlIcon from 'assets/images/skills/html.png';
 import javascriptIcon from 'assets/images/skills/js.png';
-import typescriptIcon from 'assets/images/skills/ts.png';
+import mongodbIcon from 'assets/images/skills/mongodb.png';
 import reactIcon from 'assets/images/skills/react.png';
 import reduxIcon from 'assets/images/skills/redux.svg';
+import scssIcon from 'assets/images/skills/scss.png';
 import tailwindIcon from 'assets/images/skills/tailwind.png';
-import gitIcon from 'assets/images/skills/git.png';
+import typescriptIcon from 'assets/images/skills/ts.png';
 
 export const skillArray = [
 	{ skillName: 'HTML', icon: htmlIcon },
@@ -16,6 +19,9 @@ export const skillArray = [
 	{ skillName: 'TypeScript', icon: typescriptIcon },
 	{ skillName: 'React', icon: reactIcon },
 	{ skillName: 'Redux Toolkit', icon: reduxIcon },
-	{ skillName: 'TailWindCSS', icon: tailwindIcon },
+	{ skillName: 'TailwindCSS', icon: tailwindIcon },
 	{ skillName: 'Git', icon: gitIcon },
+	{ skillName: 'Firebase', icon: firebaseIcon },
+	{ skillName: 'Express.js', icon: expressjsIcon },
+	{ skillName: 'MongoDB', icon: mongodbIcon },
 ];
